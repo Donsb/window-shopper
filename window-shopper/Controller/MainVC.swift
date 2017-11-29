cd65def3a18346a655d98e5bdd40b67a48fcec47
Donald Belliveau <donsb@icloud.com>
2017-11-28 19:45:34 -0800

    Added IB Outlets for the 2 TextFields.  Added Calculate Button in Code to the top of the iOS keyboard.

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    /*
+     IBOutlets
+     */
+    
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    /*
+     Functions
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Create UIButton attaching to the textfield's keyboard.
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 60))
+        // Set Button Colour
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.4557419206, blue: 0.3894221219, alpha: 1)
+        //Set Button Title
+        calcBtn.setTitle("Calculate", for: .normal)
+        // Set Button Title Colour
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        // Target for when the button is pressed (what function)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Calculate Button Function
+    @objc func calculate() {
+        print("Test Working")
+    }
 }
 
