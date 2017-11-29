46f046ab4ef9a8c244e5bb2763f36e9e293943fb
Donald Belliveau <donsb@icloud.com>
2017-11-29 07:33:58 -0800

    Turned on UI Unit Testing.  Created new Group folder called Model.  Added Swift file (Wage) in Model folder.

---
+++ window-shopper/Model/Wage.swift
@@ -0,0 +1,13 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Donald Belliveau on 2017-11-29.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import Foundation
+
+class Wage {
+    
+}
